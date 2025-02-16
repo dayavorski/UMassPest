251cf412a3cb97b37d2855a0e25c433545babf3e
dayavorski <dayavorski@gmail.com>
2025-02-15 22:03:02 -0500

    Add files via upload

---
+++ UPest.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+//app class
+class UPest {
+
+
+    
+}
---
+++ UmassPest.js
@@ -0,0 +1,21 @@
+//Umass Pest
+
+document.addEventListener("DOMContentLoaded", function () 
+{ 
+    const dropdowns =  document.querySelectorAll(".dropdown");
+
+dropdowns.forEach((dropdown) => 
+    {
+dropdown.addEventListener("mouseenter", () => 
+    {
+
+    dropdown.querySelector(".dropdown-content").style.display = "block";
+});
+
+dropdown.addEventListener("mouseleave", () => 
+    {
+    dropdown.querySelector(".dropdown-content").style.display = "none";
+        });
+    });
+});
+
